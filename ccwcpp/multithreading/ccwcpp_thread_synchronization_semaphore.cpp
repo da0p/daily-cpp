d850523423b5462e9feb9b311a2f95829746053a
Phuong Dao <phuong018@gmail.com>
2023-07-16 18:53:09 +0200

    [Day 13] Add c++ semaphore

---
+++ ccwcpp/multithreading/ccwcpp_thread_synchronization_semaphore.cpp
@@ -0,0 +1,42 @@
+#include <cstdlib>
+#include <iostream>
+#include <semaphore>
+#include <thread>
+#include <vector>
+
+std::vector<int> myVec{};
+
+std::counting_semaphore<1> prepareSignal(0);
+
+void
+prepareWork()
+{
+  myVec.insert(myVec.end(), {0, 1, 0, 3});
+  std::cout << "Sender: Data prepared.\n";
+  prepareSignal.release();
+}
+
+void
+completeWork()
+{
+  std::cout << "Waiter: Waiting for data.\n";
+  prepareSignal.acquire();
+  myVec[2] = 2;
+  std::cout << "Waiter: Complete the work.\n";
+  for(auto i : myVec) {
+    std::cout << i << " ";
+  }
+  std::cout << "\n";
+}
+
+int
+main(int argc, char* argv[])
+{
+  std::thread t1(prepareWork);
+  std::thread t2(completeWork);
+
+  t1.join();
+  t2.join();
+
+  return EXIT_SUCCESS;
+}
