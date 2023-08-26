3cd6deb004448924e8542e647e461aae579a57bf
Phuong Dao <phuong018@gmail.com>
2023-08-26 17:33:45 +0200

    [Day 45] Algorithm: unique

---
+++ algorithm/unique.cpp
@@ -0,0 +1,19 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+int
+main()
+{
+  std::vector<int> data{1, 1, 2, 2, 3, 4, 5, 6, 6, 6};
+
+  auto it = std::unique(data.begin(), data.end());
+  // data == {1, 2, 3, 4, 5, 6, unspec, unspec, unspec, unspec};
+
+  data.resize(std::distance(data.begin(), it));
+  // data == {1, 2, 3, 4, 5, 6};
+
+  for(auto i : data) {
+    std::cout << i << " ";
+  }
+}
