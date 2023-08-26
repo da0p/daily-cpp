ae5e6c8804d0dd3e64db129ddcebf52eeef033e2
Phuong Dao <phuong018@gmail.com>
2023-08-26 11:20:20 +0200

    [Day 45] Algorithm: stable_partition

--- algorithm/partition.cpp
+++ algorithm/partition.cpp
@@ -20,12 +20,31 @@ main()
     [threshold = 69](auto const& r) { return r.score >= threshold; }
   );
 
-  // let's print who passes
-  for(auto it = examResult.begin(); it != pp; it++) {
-    std::cout << "[PASS] " << it->name << ", " << it->score << "\n";
-  }
+  auto print = [](auto const& v, auto const& pp) {
+    // let's print who passes
+    for(auto it = v.begin(); it != pp; it++) {
+      std::cout << "[PASS] " << it->name << ", " << it->score << "\n";
+    }
 
-  for(auto it = pp; it != examResult.end(); it++) {
-    std::cout << "[FAIL] " << it->name << ", " << it->score << "\n";
-  }
+    for(auto it = pp; it != v.end(); it++) {
+      std::cout << "[FAIL] " << it->name << ", " << it->score << "\n";
+    }
+  };
+
+  print(examResult, pp);
+
+  std::cout << "\n\n";
+
+  std::vector<ExamResult> physics{
+    {"Peter", 100}, {"John", 50}, {"Harley", 75}, {"James", 69}, {"Leon", 87}};
+
+  // std::stable_partition will keep the relative order of elements in both
+  // partition while std::partition does not
+  auto omega = std::stable_partition(
+    physics.begin(),
+    physics.end(),
+    [threshold = 69](auto const& r) { return r.score >= threshold; }
+  );
+
+  print(physics, omega);
 }
