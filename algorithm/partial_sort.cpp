6be6e434d04cdfcfdfe6f4197fbe8d643797955b
Phuong Dao <phuong018@gmail.com>
2023-08-26 10:49:11 +0200

    [Day 45] Reodering repo's structure

--- algorithm/partial_sort.cpp
+++ algorithm/partial_sort.cpp
@@ -7,6 +7,12 @@ int
 main()
 {
   std::vector<int> data{9, 8, 7, 6, 5, 4, 3, 2, 1};
+
+  // std::partial_sort algorithm reorders the range's elements such that the
+  // leading sub-range is in the same order it would when fully sorted. However,
+  // the algorithm leaves the rest of the range in an unspecified order.
+  // But the benefit of partial sort is faster runtime - approximately O(n *
+  // logk), where k is the number of elements sorted.
   std::partial_sort(data.begin(), data.begin() + 3, data.end());
   // data == {1, 2, 3, --unspecified order-}
   for(auto d : data) {
--- algorithm/partial_sort_copy.cpp
+++ algorithm/partial_sort_copy.cpp
@@ -11,6 +11,9 @@ main()
   auto input = std::istream_iterator<int>(std::cin);
   auto cnt = std::counted_iterator(input, 10);
 
+  // std::partial_sort_copy algorithm has the same behavior as
+  // std::partial_sort, however, it does not operate inline. Instead, the
+  // algorithm writes the results to a second range.
   std::ranges::partial_sort_copy(
     cnt, std::default_sentinel, top.begin(), top.end(), std::greater<>{}
   );
