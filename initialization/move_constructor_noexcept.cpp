d2c7eb080b38bb96813398dfab3e413fb34c5060
Phuong Dao <phuong018@gmail.com>
2023-10-07 16:34:26 +0200

    [Daily-57] Initialization: copy and move assignment operators

--- initialization/move_constructor_noexcept.cpp
+++ initialization/move_constructor_noexcept.cpp
@@ -35,6 +35,26 @@ public:
   }
   */
 
+  Product& operator=(Product const& other)
+  {
+    if(this == &other) {
+      return *this;
+    }
+    id_ = other.id_;
+    name_ = other.name_;
+    std::cout << "operator=(copy): " << id_ << ", " << name_ << "\n";
+    return *this;
+  }
+
+  // move assignment operator
+  Product& operator=(Product&& other) noexcept
+  {
+    id_ = other.id_;
+    name_ = std::move(other.name_);
+    std::cout << "operator=(move): " << id_ << ", " << name_ << "\n";
+    return *this;
+  }
+
   std::string const& name() const { return name_; }
 
 private:
@@ -45,6 +65,7 @@ private:
 int
 main()
 {
+  std::cout << "Move ctor marked with noexcept:\n\n";
   Product tvSet{100, "tv set"};
   std::cout << tvSet.name() << " created...\n";
   Product setV2{std::move(tvSet)};
@@ -63,4 +84,27 @@ main()
   prods.emplace_back(303, "toy");
   prods.emplace_back(404, "mug");
   prods.emplace_back(505, "pencil");
+  std::cout << "\n\n";
+
+  std::cout << "Copy assignment operators:\n\n";
+  // copy and move assignment operators
+  // The compiler calls a copy constructor when you initialize an object. When
+  // an entity already exists, the program runs an assignment operation.
+  Product base{42, "base"};
+  Product first{base};    // copy ctor called!
+  Product second = first; // copy ctor called!
+  Product third{100, "third"};
+  third = second; // assignment operator called!
+  std::cout << "\n\n";
+
+  std::cout << "Move assignment operators:\n\n";
+  // Move assignment operators
+  Product base2{42, "base"};
+  Product first2{std::move(base)}; // move ctor called!
+  std::cout << "base2.name() after mvoe: " << base2.name() << "\n";
+  Product second2 = std::move(first); // move ctor called!
+  std::cout << "first2.name() after move: " << first2.name() << "\n";
+  Product third2{100, "third"};
+  third2 = std::move(second2); // assignment operator called!
+  std::cout << "second2.name() after move: " << second2.name() << "\n";
 }
